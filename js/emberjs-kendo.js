4aad9a088a4d73796947ce91541c822e7a30b8eb
Greg Babiars <gbabiars@gmail.com>
2012-07-10 22:29:32 -0700

    Added Start option
    Moved options to view level so it can be set as a binding.

--- js/emberjs-kendo.js
+++ js/emberjs-kendo.js
@@ -2,11 +2,18 @@ window.Kendo = window.Kendo || {};
 
 // Views
 Kendo.DatePicker = Ember.TextField.extend({
+	options: {},
 	format: null,
+	value: null,
 	didInsertElement: function() {
-		var options = {};
+		var options = this.get('options');
 		if(this.get('format'))
 			options.format = this.get('format');
+		if(this.get('value'))
+			var dateValue = new Date(this.get('value'));
+			options.value = dateValue;
+		if(this.get('start'))
+			options.start = this.get('start');
 		var el = this.get('element');
 		$(el).kendoDatePicker(options);
 	}
