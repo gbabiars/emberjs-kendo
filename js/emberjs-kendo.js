5158ef028f480d9e1a78e13cff6b2f80557cbcf8
Greg Babiars <gbabiars@gmail.com>
2012-07-10 21:36:23 -0700

    Added Date Format option.

--- js/emberjs-kendo.js
+++ js/emberjs-kendo.js
@@ -2,8 +2,12 @@ window.Kendo = window.Kendo || {};
 
 // Views
 Kendo.DatePicker = Ember.TextField.extend({
+	format: null,
 	didInsertElement: function() {
+		var options = {};
+		if(this.get('format'))
+			options.format = this.get('format');
 		var el = this.get('element');
-		$(el).kendoDatePicker();
+		$(el).kendoDatePicker(options);
 	}
 });
