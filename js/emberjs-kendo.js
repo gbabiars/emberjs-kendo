e1b8eb8c467162a9bea464e9493bb28302b1a698
Greg Babiars <gbabiars@gmail.com>
2012-07-11 22:53:15 -0700

    Refined didInsertElement and self executing
    Implemented depth.  The function is less explicit when adding options.
    Options is a new object every time to avoid shared state.

--- js/emberjs-kendo.js
+++ js/emberjs-kendo.js
@@ -1,20 +1,28 @@
 window.Kendo = window.Kendo || {};
 
 // Views
-Kendo.DatePicker = Ember.TextField.extend({
-	options: {},
-	format: null,
-	value: null,
-	didInsertElement: function() {
-		var options = this.get('options');
-		if(this.get('format'))
-			options.format = this.get('format');
-		if(this.get('value'))
-			var dateValue = new Date(this.get('value'));
-			options.value = dateValue;
-		if(this.get('start'))
-			options.start = this.get('start');
-		var el = this.get('element');
-		$(el).kendoDatePicker(options);
-	}
-});
+(function() {
+	var get = Ember.get, set = Ember.set;
+	Kendo.DatePicker = Ember.TextField.extend({
+		format: null,
+		value: null,
+		start: null,
+		depth: null,
+		didInsertElement: function() {
+			var fields = ['format', 'value', 'start', 'depth'],
+				options = {},
+				el = this.get('element'),
+				field;
+			for(var i = 0; i < fields.length; i++) {
+				field = fields[i];
+				if(field === 'value' && this.get('value')) {
+					options.value = new Date(this.get('value'));
+				}
+				else if(this.get(field)) {
+					options[field] = this.get(field);
+				}
+			}
+			$(el).kendoDatePicker(options);
+		}
+	});
+})();
